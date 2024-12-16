17c4c95425ee264d76a2cf9c81cdb7a34bf08fe3
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2024-12-16 13:04:04 -0800

    Update output.tf

--- output.tf
+++ output.tf
@@ -6,5 +6,5 @@ output "function_name" {
   value       = join("", aws_lambda_function.this.*.function_name)
 }
 output "lambda_function_version" {
-  value = aws_lambda_function_version.this*.version
+  value = aws_lambda_function_version.this.*.version
 }
