3bf9d2946f956c9cc9d1832a0db5effa52486415
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2024-12-16 11:50:20 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,28 @@
-
-  resource "aws_lambda_function" "this" {
-    count = var.enabled ? 1 : 0
-    filename = var.filename
-    source_code_hash = var.source_code_hash
-    function_name = var.function_name
-    role          = var.role
-    handler       = var.handler
-    runtime       = var.runtime
-    timeout       = var.timeout
-    description   = var.description
-    tags          = var.tags
-  }
+resource "aws_lambda_function" "this" {
+  count               = var.enabled ? 1 : 0
+  filename            = var.filename
+  source_code_hash    = var.source_code_hash
+  function_name       = var.function_name
+  role                = var.role
+  handler             = var.handler
+  runtime             = var.runtime
+  timeout             = var.timeout
+  description         = var.description
+  tags                = var.tags
+  publish             = true  # This ensures that a version is created every time the function is updated
+}
 
 resource "aws_lambda_permission" "allow_events_bridge_to_run_lambda" {
-    statement_id = "AllowExecutionFromCloudWatch"
-    action = "lambda:InvokeFunction"
-    function_name = join("", aws_lambda_function.this.*.function_name)
-    principal = "events.amazonaws.com"
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = join("", aws_lambda_function.this.*.function_name)
+  principal     = "events.amazonaws.com"
 }
+
+resource "aws_lambda_function_version" "this" {
+  count               = var.enabled ? 1 : 0
+  function_name       = aws_lambda_function.this[0].function_name
+  description         = "Version for ${aws_lambda_function.this[0].function_name}"
+  source_code_hash    = aws_lambda_function.this[0].source_code_hash
+}
+
