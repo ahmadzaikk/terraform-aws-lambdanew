7c9fa5d56288c64043271b301a58131b04848acd
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2024-12-17 11:36:34 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_lambda_function" "this" {
-  provider = var.region.provider
+  provider = var.region_provider
   count            = var.enabled ? 1 : 0
   filename         = var.filename
   source_code_hash = var.source_code_hash
